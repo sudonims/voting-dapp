c57f2029e793bd3a17356540f3e2eb2b5051123d
Nimish Shah <nimishshah2000@gmail.com>
2022-01-06 23:32:34 +0530

    migrations helper

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,23 +2,32 @@
 pragma solidity 0.8.11;
 
 contract SimpleStorage {
-  uint storedData;
-  mapping (string=>bool) voterID;
-  mapping (string=>bool) addressInUse;
+    uint256 storedData;
+    mapping(string => bool) public voterID;
+    mapping(string => bool) public addressInUse;
+    string[] public arr;
 
-  constructor() public {}
-  function registerNewVoter(string memory id, string memory add) public returns (string memory ret) {
-    
-    if(!voterID[id]) {
-      voterID[id] = true;
-      addressInUse[add] = true;
-      return "Success";
-    } else {
-      return "Voter already registered";
+    constructor() public {}
+
+    function registerNewVoter(string memory id, string memory add)
+        public
+        returns (string memory)
+    {
+        if (!voterID[id]) {
+            arr.push(id);
+            voterID[id] = true;
+            addressInUse[add] = true;
+            return ("success");
+        } else {
+            return ("Voter already registered");
+        }
     }
-  }
 
-  function isAddressInUse(string calldata add) public view returns (bool) {
-    return addressInUse[add] ? true : false;
-  }
+    function getAdd() public view returns (string memory) {
+        return arr[0];
+    }
+
+    function isAddressInUse(string calldata add) public view returns (bool) {
+        return addressInUse[add] ? true : false;
+    }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -5,8 +5,10 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545,
+    ganache: {
+      network_id: 5777,
+      port: 7545,
+      host: "127.0.0.1",
     },
   },
   compilers: {
